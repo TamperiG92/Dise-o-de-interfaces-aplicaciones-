61da5c2edf738762a5e072045e34b02c0a574062
generex27 <167477453+TamperiG@users.noreply.github.com>
2026-03-05 00:12:46 -0500

    add register and terms and conditions

---
+++ app/src/main/java/com/example/interfaces/activities/register_user.kt
@@ -0,0 +1,31 @@
+package com.example.interfaces.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.interfaces.R
+
+class register_user : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Navegación de vuelta al login
+        val tvIrLogin = findViewById<TextView>(R.id.tv_ir_login)
+        tvIrLogin.setOnClickListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
